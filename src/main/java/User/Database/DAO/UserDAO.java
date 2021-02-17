8bf4ff13209c3e7cf80ee85c0bc8a7fde771f66f
Denis <dp837705@gmail.com>
2021-02-17 12:53:27 +0000

    User DAO initial implementation

---
+++ src/main/java/User/Database/DAO/UserDAO.java
@@ -0,0 +1,20 @@
+package User.Database.DAO;
+
+import java.sql.Connection;
+
+public class UserDAO {
+    private final Connection connection;
+
+    public UserDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+
+    public void saveUserData() {
+
+    }
+
+    public void retrieveUserData() {
+
+    }
+}

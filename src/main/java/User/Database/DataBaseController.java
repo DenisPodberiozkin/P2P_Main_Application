968f6b0cd3fde6e581af00a3c6df4c96687cb71a
Denis <dp837705@gmail.com>
2021-02-17 12:25:21 +0000

    Added Singleton

--- src/main/java/User/Database/DataBaseController.java
+++ src/main/java/User/Database/DataBaseController.java
@@ -4,6 +4,15 @@ import java.sql.Connection;
 
 public class DataBaseController implements IDataBaseController {
 
+    private static DataBaseController instance;
+
+    public static DataBaseController getInstance() {
+        if (instance == null) {
+            instance = new DataBaseController();
+        }
+        return instance;
+    }
+
     public Connection connectToDatabase() {
         Database database = Database.getInstance();
         return database.connect("Userdata");

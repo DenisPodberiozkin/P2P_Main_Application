f189145989b536b4f583ae80ce486e84be61b4b7
Denis <dp837705@gmail.com>
2021-02-26 22:01:30 +0000

    Added Port and IP initialisation

--- src/main/java/User/NodeManager/Node.java
+++ src/main/java/User/NodeManager/Node.java
@@ -4,16 +4,19 @@ import User.Encryption.EncryptionController;
 import User.Encryption.EncryptionUtil;
 
 import java.security.PublicKey;
+import java.util.Objects;
 
 public class Node {
     private final String id;
     private String ip;
     private String publicIp;
+    private int port;
     private final PublicKey publicKey;
 
-    public Node(PublicKey publicKey) {
+    public Node(PublicKey publicKey, int port) {
         this.publicKey = publicKey;
         this.id = generateId();
+        this.port = port;
     }
 
     private String generateId() {
@@ -44,4 +47,28 @@ public class Node {
         return publicKey;
     }
 
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public Object lookup(String id) {
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Node)) return false;
+        Node node = (Node) o;
+        return id.equals(node.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/User/NodeManager/User.java
+++ src/main/java/User/NodeManager/User.java
@@ -1,5 +1,13 @@
 package User.NodeManager;
 
+import User.ConnectionsData;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.URL;
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
@@ -10,21 +18,75 @@ public class User extends Node {
     private Node successor;
     private SortedMap<String, Node> nodes;
     private PrivateKey privateKey;
+    private static User instance;
 
     public User(KeyPair keyPair) {
-        super(keyPair.getPublic());
+        this(keyPair.getPublic());
         this.privateKey = keyPair.getPrivate();
     }
 
     public User(PublicKey publicKey) {
-        super(publicKey);
+        super(publicKey, ConnectionsData.USER_SERVER_PORT);
+        initialiseIP();
+        User.instance = this;
+    }
+
+    public static User getInstance() {
+        return instance;
+    }
+
+    private void initialiseIP() {
+        try (final DatagramSocket socket = new DatagramSocket()) {
+            socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+            String ip = socket.getLocalAddress().getHostAddress();
+            String publicIp = initialisePublicIp();
+            setIp(ip);
+            setPublicIp(publicIp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String initialisePublicIp() throws IOException {
+        URL whatIsMyIp = new URL("http://checkip.amazonaws.com");
+        try (final BufferedReader in = new BufferedReader(new InputStreamReader(whatIsMyIp.openStream()))) {
+            return in.readLine();
+        }
+    }
+
+    public PrivateKey getPrivateKey() {
+        return privateKey;
     }
 
     public void setPrivateKey(PrivateKey privateKey) {
         this.privateKey = privateKey;
     }
 
-    public PrivateKey getPrivateKey() {
-        return privateKey;
+    public Node getPredecessor() {
+        return predecessor;
+    }
+
+    public void setPredecessor(Node predecessor) {
+        this.predecessor = predecessor;
+    }
+
+    public Node getSuccessor() {
+        return successor;
+    }
+
+    public void setSuccessor(Node successor) {
+        this.successor = successor;
+    }
+
+    public SortedMap<String, Node> getNodes() {
+        return nodes;
+    }
+
+    public Node getNodeFromTable(String id) {
+        return nodes.get(id);
+    }
+
+    public boolean hasNeighbours() {
+        return !nodes.isEmpty();
     }
 }

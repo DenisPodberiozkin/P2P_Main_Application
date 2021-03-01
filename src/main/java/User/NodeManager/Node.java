1a977ab6440e73b18e31ce679aab6f8295bc38c8
Denis <dp837705@gmail.com>
2021-03-01 10:17:36 +0000

    Created methods to generate Object ot JSON string

--- src/main/java/User/NodeManager/Node.java
+++ src/main/java/User/NodeManager/Node.java
@@ -2,21 +2,36 @@ package User.NodeManager;
 
 import User.Encryption.EncryptionController;
 import User.Encryption.EncryptionUtil;
+import User.JSON.NodeDeserializer;
+import User.JSON.NodeSerializer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 
 import java.security.PublicKey;
+import java.util.Base64;
 import java.util.Objects;
+import java.util.logging.Logger;
 
 public class Node {
+    private static final Logger LOGGER = Logger.getLogger(Node.class.getName());
     private final String id;
     private String ip;
     private String publicIp;
     private int port;
     private final PublicKey publicKey;
+    private static ObjectMapper objectMapper;
 
     public Node(PublicKey publicKey, int port) {
         this.publicKey = publicKey;
         this.id = generateId();
         this.port = port;
+
+        objectMapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule("NodeSerializer", new Version(1, 0, 0, null, null, null));
+        module.addSerializer(Node.class, new NodeSerializer());
+        module.addDeserializer(Node.class, new NodeDeserializer());
     }
 
     private String generateId() {
@@ -59,6 +74,29 @@ public class Node {
         return null;
     }
 
+    public static Node getNodeFromJSONSting(String jsonString) {
+        Node node = null;
+        try {
+            node = objectMapper.readValue(jsonString, Node.class);
+        } catch (JsonProcessingException e) {
+            LOGGER.warning("Error while generating Node object from JSON string");
+            e.printStackTrace();
+        }
+
+        return node;
+    }
+
+    public String getJSONString() {
+        String jsonString = "";
+        try {
+            jsonString = objectMapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            LOGGER.warning("Error while generating JSON String");
+            e.printStackTrace();
+        }
+        return jsonString;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -71,4 +109,17 @@ public class Node {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "id='" + id + '\'' +
+                ", ip='" + ip + '\'' +
+                ", publicIp='" + publicIp + '\'' +
+                ", port=" + port +
+                ", publicKey=" + Base64.getEncoder().encodeToString(publicKey.getEncoded()) +
+                '}';
+    }
 }
+
+

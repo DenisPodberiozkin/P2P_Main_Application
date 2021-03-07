af3cd1ed645c8db1371554f51fadda4e069ccabe
Denis <dp837705@gmail.com>
2021-03-07 02:37:10 +0000

    Added remove unreachable last connected node token

--- src/main/java/User/CommunicationUnit/Client/OutboundTokens.java
+++ src/main/java/User/CommunicationUnit/Client/OutboundTokens.java
@@ -1,7 +1,7 @@
 package User.CommunicationUnit.Client;
 
 public enum OutboundTokens {
-    GET_LAST_NODE("GETLN"), SET_LAST_NODE("SETLN");
+    GET_LAST_NODE("GETLN"), SET_LAST_NODE("SETLN"), REMOVE_UNREACHABLE_LAST_CONNECTED_NODE("REMOVE");
 
     private final String token;
 

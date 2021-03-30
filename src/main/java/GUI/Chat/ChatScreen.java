5a2ab2a3b358826c57265bed8c42b78816b6cac7
Denis <dp837705@gmail.com>
2021-03-30 22:43:35 +0100

    Added chat screen GUI

---
+++ src/main/java/GUI/Chat/ChatScreen.java
@@ -0,0 +1,16 @@
+package GUI.Chat;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChatScreen implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+
+
+}
